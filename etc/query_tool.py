bdff1780c271d49704476e1524ed9ed77a7b15b8
David Bielik <dvdblk@users.noreply.github.com>
2023-06-26 01:10:21 +0200

    feat(query_tool): add plot commands (#103)
    * feat: add plot_overview command
    
    * feat: add db records plot
    
    * feat: finish plot_overview
    
    * feat: update plot_overview y labels
    
    * feat: add n logs per contract
    
    * chore: remove comment

--- etc/query_tool.py
+++ etc/query_tool.py
@@ -1,8 +1,149 @@
 """Script for executing pre defined SQL queries / statements."""
 import argparse
 import asyncio
+import json
+from datetime import datetime
 
 import asyncpg
+import matplotlib.pyplot as plt
+
+
+async def cmd_plot_overview(conn, args):
+    """ """
+    print(datetime.now())
+
+    # Get the number of transactions
+    n_txs_record = await conn.fetchrow(
+        "SELECT count(*) FROM (SELECT transaction_hash FROM eth_transaction) AS tx"
+    )
+    n_txs = n_txs_record["count"]
+    print(f"Number of transactions: {n_txs}")
+
+    # Get the number of blocks
+    n_blocks_record = await conn.fetchrow(
+        "SELECT count(*) FROM (SELECT block_number FROM eth_block) AS blk"
+    )
+    n_blocks = n_blocks_record["count"]
+    print(f"Number of blocks: {n_blocks}")
+
+    # Get the number of internal transactions
+    n_internal_txs_record = await conn.fetchrow(
+        "SELECT COUNT(*) FROM eth_internal_transaction"
+    )
+    n_internal_txs = n_internal_txs_record["count"]
+    print(f"Number of internal transactions: {n_internal_txs}")
+
+    # Get the number of logs
+    n_logs_record = await conn.fetchrow("SELECT COUNT(*) FROM eth_transaction_logs")
+    n_logs = n_logs_record["count"]
+    print(f"Number of logs: {n_logs}")
+
+    # Get sizes of tables
+    table_sizes_record = await conn.fetch(
+        """
+        select
+        table_name,
+        pg_size_pretty(pg_relation_size(quote_ident(table_name))) as size_in_db,
+        pg_relation_size(quote_ident(table_name))
+        from information_schema.tables
+        where table_schema = 'public'
+        AND (table_name = 'eth_block' OR table_name = 'eth_transaction' OR table_name = 'eth_internal_transaction' OR table_name = 'eth_transaction_logs')
+        ORDER BY 3 ASC
+        """
+    )
+    table_sizes = [record["size_in_db"] for record in table_sizes_record]
+    print(table_sizes)
+
+    # Save as figure 1
+    fig, ax = plt.subplots()
+    bar_names = ("Blocks", "External Transactions", "Logs", "Internal Transactions")
+    counts = [n_blocks, n_txs, n_logs, n_internal_txs]
+
+    # ax.bar(bar_names, counts)
+    bars = ax.barh(bar_names, counts)
+    ax.bar_label(bars, fmt="{:,}", padding=16)
+    ax.set_ylabel("Record Type")
+    ax.set_xlabel("# of records in DB")
+    ax.set_title("Database overview")
+    ax.set_xscale("log")
+    ax.set_xlim(1, 4e12)
+
+    for bar, size_in_db in zip(ax.patches, table_sizes):
+        ax.text(
+            2,
+            bar.get_y() + bar.get_height() / 2,
+            size_in_db,
+            color="white",
+            ha="left",
+            va="center",
+        )
+
+    fig.savefig(f"{args.output_dir}/database_overview.png", bbox_inches="tight")
+
+    print(datetime.now())
+
+
+async def cmd_plot_others(conn, args):
+    """"""
+    # Number of logs per contract
+    n_logs_per_contract_records = await conn.fetch(
+        """
+        SELECT address, count(*) AS n_logs
+        FROM eth_transaction_logs
+        GROUP BY address
+        ORDER BY n_logs DESC
+        """
+    )
+    n_logs_per_contract = {
+        r.get("address"): r.get("n_logs") for r in n_logs_per_contract_records
+    }
+
+    # Load config
+    cfg = json.load(open(args.config_path))
+    cfg_dict = {c["address"].lower(): c for c in cfg["data_collection"][0]["contracts"]}
+    symbols = []
+    contract_categories = []
+    contract_colors = {
+        "UniSwapV2Pair": "lightskyblue",
+        "erc20": "green",
+        "erc721": "orange",
+    }
+    n_logs_per_symbol = []
+
+    def remove_prefix(s, prefix):
+        return s[len(prefix) :] if s.startswith(prefix) else s
+
+    for address, n_logs in n_logs_per_contract.items():
+        address = address.lower()
+        if cfg_dict.get(address):
+            symbol = remove_prefix(cfg_dict[address]["symbol"], "UniSwap V2 Pair ")
+            symbols.append(symbol)
+            contract_categories.append(cfg_dict[address]["category"])
+            n_logs_per_symbol.append(n_logs)
+
+    # Save as figure 1
+    fig, ax = plt.subplots(figsize=(20, 10))
+
+    for i, symbol in enumerate(symbols):
+        color = contract_colors[contract_categories[i]]
+        ax.bar(symbol, n_logs_per_symbol[i], color=color, label=contract_categories[i])
+
+    ax.set_ylabel("# of logs")
+    ax.set_xlabel("Symbol")
+    ax.set_title("Number of logs per contract")
+    ax.set_yscale("log")
+    ax.set_xticklabels(symbols, rotation=90)
+    labels = list(contract_colors.keys())
+    ax.legend(
+        [plt.Rectangle((0, 0), 1, 1, color=contract_colors[label]) for label in labels],
+        labels,
+        loc="upper right",
+        prop={"size": 14},
+    )
+
+    fig.savefig(
+        f"{args.output_dir}/database_logs_per_contract.png", bbox_inches="tight"
+    )
 
 
 async def cmd_event(conn, args):
@@ -100,6 +241,28 @@ async def main():
     )
     parser_supply.add_argument(
         "-e", "--end-block", help="Ending block, not included.", type=int, default=None
+    )
+
+    # 'plot_overview' command
+    parser_plot = subparsers.add_parser(
+        name="plot_overview",
+        description="Plot overview graphs from data in the database",
+    )
+    parser_plot.set_defaults(func=cmd_plot_overview)
+    parser_plot.add_argument(
+        "-o", "--output-dir", help="Output directory name", required=True
+    )
+
+    # 'plot_others' command
+    parser_plot = subparsers.add_parser(
+        name="plot_others", description="Plot some graphs from data in the database"
+    )
+    parser_plot.set_defaults(func=cmd_plot_others)
+    parser_plot.add_argument(
+        "-o", "--output-dir", help="Output directory name", required=True
+    )
+    parser_plot.add_argument(
+        "-c", "--config-path", help="Path to the config file", required=True
     )
 
     # Get the CLI arguments
