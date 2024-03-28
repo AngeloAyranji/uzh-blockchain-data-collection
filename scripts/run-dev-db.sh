fd446f3b910e5c6fba1755ee655cb222115eae11
AngeloAyranji <angeloayranji@gmail.com>
2024-03-28 23:41:18 +0200

    fixed run-dev-db.sh script

--- scripts/run-dev-db.sh
+++ scripts/run-dev-db.sh
@@ -2,9 +2,12 @@
 
 set -e
 
-export PROJECT_NAME="$PROJECT_NAME-dev"
-
 source scripts/util/prepare-env.sh
+
+export PROJECT_NAME="$PROJECT_NAME-dev"
+export DATA_DIR="$DATA_DIR-dev"
+export KAFKA_DATA_DIR="$KAFKA_DATA_DIR-dev"
+
 source scripts/util/prepare-data-dir.sh $DATA_DIR $KAFKA_DATA_DIR $KAFKA_N_PARTITIONS
 source scripts/util/compose-cleanup.sh
 
