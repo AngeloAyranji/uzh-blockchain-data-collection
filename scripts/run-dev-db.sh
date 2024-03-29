51be5c0ed94fa8353b99c5e000cd7879118768ab
AngeloAyranji <angeloayranji@gmail.com>
2024-03-29 14:37:29 +0200

    feat: added new script to prepare db data dir

--- scripts/run-dev-db.sh
+++ scripts/run-dev-db.sh
@@ -6,14 +6,9 @@ source scripts/util/prepare-env.sh
 
 export PROJECT_NAME="$PROJECT_NAME-dev"
 export DATA_DIR="$DATA_DIR-dev"
-export KAFKA_DATA_DIR="$KAFKA_DATA_DIR-dev"
 
+source scripts/util/prepare-db-data-dir.sh $DATA_DIR
 source scripts/util/compose-cleanup.sh
-
-# Prepare the data directory for db only
-mkdir -p \
-    $DATA_DIR/postgresql-data
-chown -R $DATA_UID:$DATA_GID $DATA_DIR
 
 # Start the db
 docker compose \
