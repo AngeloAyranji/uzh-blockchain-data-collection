03119f60b0df1c59b777d20ea023d0c1046042a7
AngeloAyranji <angeloayranji@gmail.com>
2024-03-29 14:20:37 +0200

    added a script to prepare data dir for db only

--- scripts/run-dev-db.sh
+++ scripts/run-dev-db.sh
@@ -7,10 +7,13 @@ source scripts/util/prepare-env.sh
 export PROJECT_NAME="$PROJECT_NAME-dev"
 export DATA_DIR="$DATA_DIR-dev"
 export KAFKA_DATA_DIR="$KAFKA_DATA_DIR-dev"
-export KAFKA_N_PARTITIONS=4
 
-source scripts/util/prepare-data-dir.sh $DATA_DIR $KAFKA_DATA_DIR $KAFKA_N_PARTITIONS
 source scripts/util/compose-cleanup.sh
+
+# Prepare the data directory for db only
+mkdir -p \
+    $DATA_DIR/postgresql-data
+chown -R $DATA_UID:$DATA_GID $1
 
 # Start the db
 docker compose \
