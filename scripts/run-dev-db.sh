28a2d9223b97cdd7a72ea11037178a3ca8adf181
AngeloAyranji <angeloayranji@gmail.com>
2024-03-29 11:58:18 +0200

    fixed kafka partitions to 4 when read dev db

--- scripts/run-dev-db.sh
+++ scripts/run-dev-db.sh
@@ -7,6 +7,7 @@ source scripts/util/prepare-env.sh
 export PROJECT_NAME="$PROJECT_NAME-dev"
 export DATA_DIR="$DATA_DIR-dev"
 export KAFKA_DATA_DIR="$KAFKA_DATA_DIR-dev"
+export KAFKA_N_PARTITIONS=4
 
 source scripts/util/prepare-data-dir.sh $DATA_DIR $KAFKA_DATA_DIR $KAFKA_N_PARTITIONS
 source scripts/util/compose-cleanup.sh
